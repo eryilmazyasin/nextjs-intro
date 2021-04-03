39ef241273f182a4e7375833819be6de4d707a60
Yasin Eryılmaz <yasineryilmazfb@gmail.com>
2021-04-04 00:35:02 +0300

    Initial commit

---
+++ components/Image.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+const MyImage = () => {
+  <Image>
+    src="/images/profile.png"
+    height={144}
+    width={144}
+    alt="Yasin Eryılmaz"
+  </Image>
+}
+
+export default MyImage
